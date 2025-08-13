c120018eb6fe8c3985c047750618bfb6f8b3e9bc
IbrahimAhmad20 <ibrahimmohseen@gmail.com>
2025-08-14 01:05:34 +0500

    chore: cleanup unused variable to fix production build

--- src/App.js
+++ src/App.js
@@ -31,11 +31,6 @@ export default function App() {
     </div>
   );
 }
-const initialItems = [
-  { id: 1, description: "Passports", quantity: 2, packed: false },
-  { id: 2, description: "Socks", quantity: 12, packed: false },
-  { id: 3, description: "ToothBrush", quantity: 1, packed: true },
-];
 
 function Logo() {
   return <h1>🌴 FAR AWAY 👜 </h1>;
